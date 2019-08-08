49705f77a675d2aa25977fda56e6152a7266a7ec
Mikael <mikaelsantellano@hotmail.com>
2019-08-08 12:25:18 -0300

    Exercicio resolvido
    Exercicio resolvido retangulo

---
+++ ExRevisaoAula/src/exRetangulo/InterfaceRetangulo.java
@@ -0,0 +1,8 @@
+package exRetangulo;
+
+public class InterfaceRetangulo {
+	public static void main(String[] args) {
+		MenuRetangulo menuRetangulo = new MenuRetangulo();
+		menuRetangulo.menuRentangulo();
+	}
+}
